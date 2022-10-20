d97da05371e2aeb9fa57f3a8c4d281febe5b13ff
Your Name <you@example.com>
2022-10-19 20:11:06 -0600

    project setup

--- docs/src/featureToggle.js
+++
@@ -1,6 +0,0 @@
-// need to use commonjs export so that docs/packages/markdown can use
-module.exports = {
-  enable_website_banner: false,
-  enable_toc_banner: true,
-  enable_docsnav_banner: false,
-};
